a518804af6090e6136f7f92dc10590cd21a6030b
Uma <buldog52@gmail.com>
2020-07-06 22:50:23 +0300

    Added some corrections to project

--- src/components/salary-filter/salary-filter.js
+++ src/components/salary-filter/salary-filter.js
@@ -4,39 +4,24 @@ import rangesliderJs from 'rangeslider-js';
 const leftSlider = $('.multi-range__left');
 const rightSlider = $('.multi-range__right');
 
-const upperSlider = document.querySelector('.multi-range__right');
-const lowerSlider = document.querySelector('.multi-range__left');
-let upperVal = parseInt(upperSlider.value);
-let lowerVal = parseInt(lowerSlider.value);
+if ( leftSlider.length > 0 ) {
 
   rangesliderJs.create(leftSlider, {
     polyfill: false,
     onSlide: (value, percent, position) => {
       document.querySelector('.salary-filter__salary-min').innerHTML = value;
-
-
-      if (value > upperVal - 200) {
-        upperSlider.value = value + 200;
-
-        if (upperVal === upperSlider.max) {
-          lowerSlider.value = parseInt(upperSlider.max) - 200;
-        }
-
-      }
     }
   });
+
+}
+
+if ( rightSlider.length > 0 ) {
 
   rangesliderJs.create(rightSlider, {
     polyfill: false,
     onSlide: (value, percent, position) => {
       document.querySelector('.salary-filter__salary-max').innerHTML = value;
-
-      if (value < lowerVal + 200) {
-        lowerSlider.value = value - 200;
-
-        if (lowerVal === lowerSlider.min) {
-          upperSlider.value = 200;
-        }
-      }
     }
   });
+
+}
--- src/components/vacancy/vacancy.js
+++ src/components/vacancy/vacancy.js
@@ -3,7 +3,6 @@ import $ from 'jquery';
 $('.vacancy__thumbler-toggle').click(function(e) {
   const parentBlock = $(e.target).parents('.vacancy');
   const title = parentBlock.children().find('.candidate__name');
-  console.log(title);
   const thumbler = parentBlock.children().find('.vacancy__thumbler-toggle');
 
   if ( thumbler.is(":checked")) {
