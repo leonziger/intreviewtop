77d7bb0fc37a9f58efa834fd2ee89995bdcfde8d
Uma <buldog52@gmail.com>
2020-07-15 15:13:55 +0300

    Added new blocks to subscribes page

--- src/components/subscribes/subscribes.js
+++ src/components/subscribes/subscribes.js
@@ -4,15 +4,18 @@ $('.subscribes__activation-title').click(function(e){
   const parentBlock = $(e.target).parents('.subscribes__block');
   const title = parentBlock.children().find('.subscribes__activation-title');
   const thumbler = parentBlock.children().find('.subscribes__activation-toggle');
+  const add = parentBlock.children('.subscribes__add');
   const form = parentBlock.children('.subscribes-form');
 
   if ( title.hasClass('activated') ) {
     title.removeClass('activated');
     thumbler.prop('checked', false);
+    add.addClass('gray-block');
     form.addClass('transparent');
   } else {
     title.addClass('activated');
     thumbler.prop('checked', true);
+    add.removeClass('gray-block');
     form.removeClass('transparent');
   }
 
@@ -22,11 +25,23 @@ $('.subscribes__activation-toggle').click(function(e) {
   const parentBlock = $(e.target).parents('.subscribes__block');
   const title = parentBlock.children().find('.subscribes__activation-title');
   const thumbler = parentBlock.children().find('.subscribes__activation-toggle');
+  const add = parentBlock.children('.subscribes__add');
   const form = parentBlock.children('.subscribes-form');
 
   if ( thumbler.is(":checked")) {
+    add.removeClass('gray-block');
     form.removeClass('transparent');
   } else {
+    add.addClass('gray-block');
     form.addClass('transparent');
+  }
+});
+
+$('.subscribes__add').click(function(e){
+  const parentBlock = $(e.target).parents('.subscribes__block');
+  const add = parentBlock.children('.subscribes__add');
+
+  if( add.length > 0) {
+    add.toggleClass('subscribed');
   }
 });
